919cdf4c837bea91204fd66ec8ec6a54f6e883ab
Seth Farrar <seth@ai-sys.com>
2018-05-03 13:21:31 -0500

    Starts multiscene example

---
+++ Assets/samples/Multiscene/Scripts/MultiSceneManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[HelpURL("https://www.youtube.com/watch?v=KRmqy22z0SM")]
+public class MultiSceneManager : MonoBehaviour {
+
+	public string initialScene;
+
+	public static MultiSceneManager Instance;
+
+	void Awake(){
+		if(Instance!=null){
+			Destroy(this.gameObject);
+		}
+
+		Instance = this;
+		DontDestroyOnLoad(this.gameObject);
+		LoadScene(initialScene);
+	}
+
+	void Update () {
+		
+	}
+
+	public void LoadScene(string name){
+		SceneManager.LoadSceneAsync(name, LoadSceneMode.Additive);
+	}
+
+}
---
+++ Assets/samples/Multiscene/Scripts/PlayerMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed = 5f;
+
+	// Update is called once per frame
+	void Update () {
+		Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, 0);
+
+		transform.position += input * speed * Time.deltaTime;
+
+		
+	}
+}
